585ebc6200b9146d8b5ad0a3c66965b3c3d02f77
Jéssica Dias de Moraes <jessica_diasm@outlook.com>
2021-09-08 21:42:29 -0300

    Create ex1.py
    Starting exercise 01

---
+++ Week1/ex1/ex1.py
@@ -0,0 +1,9 @@
+print('Hi there Mr. User!') 
+
+user_name = input('What should I call you? ') 
+user_age = input('and how old is ' + user_name + ' ? ') 
+  
+if int(user_age) < 18: 
+    print("oh no... too young for me...") 
+
+print("Cya later!") 
